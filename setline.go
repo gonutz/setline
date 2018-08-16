1e60a1cc45a2e018c856a5029bc5939aac03498a
Lars <lars.scheme@gmail.com>
2018-08-16 13:06:40 +0200

    Initial commit

---
+++ setline.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strconv"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		panic(`expecting two arguments:
+  the line index (starting at 0)
+  the new line to set`)
+	}
+	i, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic("first argument must be the line index (starting at 0)")
+	}
+	newLine := []byte(os.Args[2])
+	data, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		panic("unable to read stdin: " + err.Error())
+	}
+	lines := bytes.Split(data, []byte{'\n'})
+	if 0 <= i && i < len(lines) {
+		line := lines[i]
+		if bytes.HasSuffix(line, []byte{'\r'}) {
+			newLine = append(newLine, '\r')
+		}
+		lines[i] = newLine
+		output := bytes.Join(lines, []byte{'\n'})
+		fmt.Printf("%s", output)
+	}
+}
